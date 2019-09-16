a2f06c1e4a2f3bfa1c2c69a73a8ec9674facdc6d
adriancornejo <55298555+adriancornejo@users.noreply.github.com>
2019-09-16 09:58:38 -0500

    Update main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
 int main() {
+    cout << "Ingrese la zona: \n";
+    cout<<"1: Super VIP\n"<<"2: VIP\n"<<"3: Preferencial\n"<<"4: General\n";
+    int opcion = 0;
+    int p = 0;
+    cin >> opcion;
 
-
-// ToDO
-
-  return 0;
+    switch(opcion)
+    {
+        case 1: cout << "¿Es cliente claro?\n"<<"1: Sí\n"<<"2: No\n";
+        cin>>p;
+        if(p==1){
+            cout<<"El precio es de 169.6 soles";
+        }
+        else
+            cout<<"El precio es de 212 soles";
+            break;
+        case 2: cout << "¿Es cliente claro?\n"<<"1: Sí\n"<<"2: No\n";
+        cin>>p;
+        if(p==1){
+            cout<<"El precio es de 136 soles";
+        }
+        else
+            cout<<"El precio es de 170 soles";
+            break;
+        case 3: cout << "¿Es cliente claro?\n"<<"1: Sí\n"<<"2: No\n";
+        cin>>p;
+        if(p==1){
+            cout<<"El precio es de 108.8 soles";
+        }
+        else
+            cout<<"El precio es de 136 soles";
+            break;
+        case 4: cout << "¿Es cliente claro?\n"<<"1: Sí\n"<<"2: No\n";
+        cin>>p;
+        if(p==1){
+            cout<<"El precio es de 47.2 soles";
+        }
+        else
+            cout<<"El precio es de 59 soles";
+            break;
+        default: cout << "No es una zona válida";
+    }
+    return 0;
 }
